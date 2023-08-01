46ac832141c4b9df709606c81f4280b5d308aa7c
熏晴微穗 <825024711@qq.com>
2023-08-01 21:35:10 +0800

    笔记更新

--- src/main/java/cn/vincent/controller/DesignController.java
+++ src/main/java/cn/vincent/controller/DesignController.java
@@ -1,9 +1,11 @@
 package cn.vincent.controller;
 
+import cn.hutool.core.util.IdUtil;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/Design")
 public class DesignController {
+
 }
