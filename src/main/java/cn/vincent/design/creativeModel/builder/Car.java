41034d45072a3c374d32eb8f6f624ffaac56bcc2
c_mengziming <825024711@qq.com>
2023-08-09 17:45:01 +0800

    建造者模式代码实现 笔记更新

---
+++ src/main/java/cn/vincent/design/creativeModel/builder/Car.java
@@ -0,0 +1,17 @@
+package cn.vincent.design.creativeModel.builder;
+
+import lombok.Data;
+
+@Data
+public class Car {
+
+    // 品牌
+    String brand;
+
+    // 汽车类型
+    String carType;
+
+    // 驱动
+    int actuate;
+
+}
---
+++ src/main/java/cn/vincent/design/creativeModel/builder/main.java
@@ -0,0 +1,11 @@
+package cn.vincent.design.creativeModel.builder;
+
+/**
+ * 创建型 - 建造者模式
+ */
+public class main {
+
+    public static void main(String[] args) {
+        Car builder = CarBuilder.builder("永久牌");
+    }
+}
